2667021e1c44f6193a511bcd2c07a65ae227f68d
a-mariam <mariiam22222@gmail.com>
2024-04-15 16:34:28 +0100

    fixing models

--- src/main/java/ecommerce/example/certify_v1/models/Certificate.java
+++ src/main/java/ecommerce/example/certify_v1/models/Certificate.java
@@ -1,12 +1,12 @@
 package ecommerce.example.certify_v1.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @Setter
 @Getter
@@ -21,6 +21,6 @@ public class Certificate {
     private String universityId;
     private String title;
     private String description;
-//    private
-    
+    @ElementCollection
+    private List<String> files;
 }
